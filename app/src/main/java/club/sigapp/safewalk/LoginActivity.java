c74e607a52bba4f54a890350a352c15dd9dc127e
Josh Kassman <jkassman2@gmail.com>
2019-01-08 20:02:27 -0500

    Renamed main activity

--- app/src/main/java/club/sigapp/safewalk/LoginActivity.java
+++ app/src/main/java/club/sigapp/safewalk/LoginActivity.java
@@ -3,7 +3,7 @@ package club.sigapp.safewalk;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
