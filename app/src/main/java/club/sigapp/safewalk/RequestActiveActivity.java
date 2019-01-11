6ee4f81e914616abdcb34e7ba7aaeff895d09c0f
Artem Ilyumzhinov <ilumzhinov@gmail.com>
2019-01-10 21:20:08 -0500

    Placeholder activities + GUI added
    No logic for switching between activities was implemented. ActionBar in the AppTheme was set to NoActionbar

---
+++ app/src/main/java/club/sigapp/safewalk/RequestActiveActivity.java
@@ -0,0 +1,15 @@
+package club.sigapp.safewalk;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RequestActiveActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_request_active);
+    }
+}
