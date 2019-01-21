bfb175390c52767c2db3310050d98b15b9675495
Artem Ilyumzhinov <ilumzhinov@gmail.com>
2019-01-20 22:27:28 -0500

    Safewalk Info fragment designed. Changes to color-scheme of the app

--- app/src/main/java/club/sigapp/safewalk/SafeWalkInfoFragment.java
+++ app/src/main/java/club/sigapp/safewalk/SafeWalkInfoFragment.java
@@ -1,11 +1,16 @@
 package club.sigapp.safewalk;
 
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 public class SafeWalkInfoFragment extends Fragment
 {
@@ -17,4 +22,39 @@ public class SafeWalkInfoFragment extends Fragment
         return inflater.inflate(R.layout.fragment_safe_walk_info, container, false);
     }
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState)
+    {
+        super.onViewCreated(view, savedInstanceState);
+
+        Button btnSafeWalkNumber = getView().findViewById(R.id.btnSafeWalkNumber),
+                btnEmergency = getView().findViewById(R.id.btnEmergency);
+
+        btnSafeWalkNumber.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                // Get a string resource
+                String number = "tel:" + getResources().getString(R.string.safewalk_number);
+
+                // Make an implicit intent and launch the dialer app
+                Intent callIntent = new Intent(Intent.ACTION_DIAL, Uri.parse(number));
+                getActivity().startActivity(callIntent);
+            }
+        });
+
+        btnEmergency.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                String number = "tel:" + getResources().getString(R.string.emergency_number);
+
+                // Make an implicit intent and launch the dialer app
+                Intent callIntent = new Intent(Intent.ACTION_DIAL, Uri.parse(number));;
+                getActivity().startActivity(callIntent);
+            }
+        });
+    }
 }
